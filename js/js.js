f20f29265cf153102ed6c13fcc0f2b98f8d84b5d
wangli <1626408397@qq.com>
2017-09-27 09:02:15 +0800

    aaa

---
+++ js/js.js
@@ -0,0 +1,13 @@
+document.documentElement.style.fontSize=window.innerWidth/375*50+"px";
+// var app = angular.module("app", []);
+// app.controller("controller", function() {
+
+// })
+// app.directive("directive", function() {
+//     return {
+//         restrict: "ECMA",
+//         template: "<h1>aaaaaaaaa</h1>",
+//         replace: true,
+//         scope: true
+//     }
+// })
